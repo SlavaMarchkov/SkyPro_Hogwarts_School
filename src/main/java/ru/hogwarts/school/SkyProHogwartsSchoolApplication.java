25afa3ff532bc8c95a60b45e8ec2007d17a87f0f
Viacheslav Marchkov <slavamarchkov@gmail.com>
2023-06-18 11:31:33 +0700

    Шаг 1. Исходное состояние проекта

---
+++ src/main/java/ru/hogwarts/school/SkyProHogwartsSchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProHogwartsSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProHogwartsSchoolApplication.class, args);
+    }
+
+}
