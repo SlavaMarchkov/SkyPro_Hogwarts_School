d7c1ebca598ceeb4595ca9a4c4830ccbc28c6e57
Viacheslav Marchkov <slavamarchkov@gmail.com>
2023-06-18 12:31:27 +0700

    Шаг 3. Создает сервисы для моделей

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,36 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+
+@Service
+public class StudentService {
+
+    private final HashMap<Long, Student> students = new HashMap<>();
+    private long count = 0;
+
+    public Student create(Student student) {
+        student.setId(count++);
+        students.put(student.getId(), student);
+        return student;
+    }
+
+    public Student read(long id) {
+        return students.get(id);
+    }
+
+    public Student update(Student student) {
+        if (!students.containsKey(student.getId())) {
+            return null;
+        }
+        students.put(student.getId(), student);
+        return student;
+    }
+
+    public Student delete(long id) {
+        return students.remove(id);
+    }
+
+}
