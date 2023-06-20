bd564eaae3817e4ced0597d421b3f1646c4845c1
Viacheslav Marchkov <slavamarchkov@gmail.com>
2023-06-20 09:18:18 +0700

    Правки по результатам разбора ДЗ по видеоуроку

--- src/main/java/ru/hogwarts/school/Application.java
+++ src/main/java/ru/hogwarts/school/Application.java
@@ -1,13 +1,15 @@
 package ru.hogwarts.school;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkyProHogwartsSchoolApplication {
+@OpenAPIDefinition
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SkyProHogwartsSchoolApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,48 +1,19 @@
 package ru.hogwarts.school.service;
 
-import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 
-@Service
-public class StudentService {
+public interface StudentService {
 
-    private final HashMap<Long, Student> students = new HashMap<>();
-    private long count = 0;
+    Student addStudent(Student student);
 
-    public Student create(Student student) {
-        student.setId(count++);
-        students.put(student.getId(), student);
-        return student;
-    }
+    Student findStudent(long id);
 
-    public Student read(long id) {
-        return students.get(id);
-    }
+    Student editStudent(long id, Student student);
 
-    public Student update(Student student) {
-        if (!students.containsKey(student.getId())) {
-            return null;
-        }
-        students.put(student.getId(), student);
-        return student;
-    }
+    void deleteStudent(long id);
 
-    public Student delete(long id) {
-        return students.remove(id);
-    }
-
-    public Collection<Student> filterByAge(int age) {
-        ArrayList<Student> filteredStudents = new ArrayList<>();
-        for (Student student : students.values()) {
-            if (student.getAge() == age) {
-                filteredStudents.add(student);
-            }
-        }
-        return filteredStudents;
-    }
+    Collection<Student> filterByAge(int age);
 
 }
