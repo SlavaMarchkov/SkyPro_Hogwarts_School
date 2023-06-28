a9ef7fdd1687be83ce14c95172f7db2757e5fc84
Viacheslav Marchkov <slavamarchkov@gmail.com>
2023-06-28 23:32:02 +0700

    Реализовывает чтение файлов с локального диска

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -10,7 +10,12 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.service.AvatarService;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping(path = "/avatar")
@@ -46,6 +51,24 @@ public class AvatarController {
                 .status(HttpStatus.OK)
                 .headers(headers)
                 .body(avatar.getData());
+    }
+
+    @GetMapping(value = "{studentId}/avatar-from-file")
+    public void downloadAvatar(@PathVariable Long studentId,
+                               HttpServletResponse response
+    ) throws IOException {
+        Avatar avatar = avatarService.findAvatar(studentId);
+        Path path = Path.of(avatar.getFilePath());
+
+        try (
+                InputStream is = Files.newInputStream(path);
+                OutputStream os = response.getOutputStream();
+        ) {
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            is.transferTo(os);
+        }
     }
 
 }
