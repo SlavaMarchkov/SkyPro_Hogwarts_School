5eb909000d63613810d49866d9647169b42414db
Viacheslav Marchkov <slavamarchkov@gmail.com>
2023-06-28 14:00:57 +0700

    Реализовывает эндпоинт в контроллере для загрузки аватара

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -1,9 +1,13 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.service.AvatarService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "/avatar")
@@ -14,6 +18,17 @@ public class AvatarController {
     @Autowired
     public AvatarController(final AvatarService avatarService) {
         this.avatarService = avatarService;
+    }
+
+    @PostMapping(value = "{studentId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long studentId,
+                                               @RequestParam MultipartFile avatar
+    ) throws IOException {
+        if (avatar.getSize() >= 1024 * 300) {
+            return ResponseEntity.badRequest().body("Avatar is too big");
+        }
+        avatarService.uploadAvatar(studentId, avatar);
+        return ResponseEntity.ok().build();
     }
 
 }
